e8c32c92e4249ef0c90c18c08d3a13f48b896fb6
Yixin Yuan <yixin.wulawula@gmail.com>
2022-01-05 11:42:38 +0100

    Update Read power.py

--- Result/Read power.py
+++ Result/Read power.py
@@ -1,11 +1,11 @@
 import time
 import serial
 ser = serial.Serial(
-    port='/dev/ttyACM0',                   #端口号
+    port='/dev/ttyACM0',                   
     baudrate=115200,
-    parity=serial.PARITY_ODD,      # 校验位 
-    stopbits=serial.STOPBITS_TWO,  # 停止位
-    bytesize=serial.SEVENBITS      # 数据位
+    parity=serial.PARITY_ODD,      
+    stopbits=serial.STOPBITS_TWO,  
+    bytesize=serial.SEVENBITS      
 )
 data = ''
 while True:
@@ -14,7 +14,7 @@ while True:
     ct = time.ctime(t)
     print(ct, ':')
     print(data)
-    f = open('vnf_1.csv', 'a')#数据保存的路径
+    f = open('vnf_1.csv', 'a')
     f.writelines(ct)
     f.writelines(',')
     f.writelines(data.decode('utf-8'))
